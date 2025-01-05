8b118625d568e80cd3e3c9386bd9e2593efe1054
Helena <gomel-offis@mail.ru>
2025-01-05 16:02:51 +0300

    feat: add task carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,23 +5,32 @@ const HALF_LIFE_PERIOD = 5730;
 
 /**
  * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ * givenc
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  console.log(sampleActivity, typeof sampleActivity);
+  if (
+    typeof sampleActivity !== 'string' ||
+    Number(sampleActivity) <= 0 ||
+    isNaN(+sampleActivity)
+  )
+    return false;
+  // console.log((5730 * Math.log(15 / +sampleActivity)) / 0.693);
+  return Math.ceil((5730 * Math.log(15 / +sampleActivity)) / 0.693);
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,41 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (str == true) console.log('str=', str, options.addition + '', options);
+  options.separator = options.separator || '+';
+  options.additionSeparator = options.additionSeparator || '|';
+  options.repeatTimes = options.repeatTimes || 1;
+  options.additionRepeatTimes = options.additionRepeatTimes || 1;
+
+  options.addition = options.addition || '';
+
+  let add = Array.from(
+    { length: options.additionRepeatTimes },
+    () => options.addition + ''
+  ).join(options.additionSeparator);
+  if (str == true)
+    console.log('str=', String(str) + '', add, options.addition + '', options);
+  return Array.from({ length: options.repeatTimes }, () => str + '' + add).join(
+    options.separator
+  );
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
