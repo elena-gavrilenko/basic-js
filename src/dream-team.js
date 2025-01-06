19c5110ddf3a0ed3811c0342207f1af6567ae166
Helen <gomel-offis@mail.ru>
2025-01-06 13:08:47 +0300

    fix: fix task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -16,27 +16,26 @@ const { NotImplementedError } = require('../extensions/index.js');
 function createDreamTeam(members) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  // let arrNew = [];
-  // if (members == null) return false;
-  // for (let i = 0; i < members.length; i++) {
-  //   if (typeof members[i] == 'string') {
-  //     for (let j = 0; j < members[i].length; j++) {
-  //       console.log(members[i][j]);
-  //       if (members[i][j] == ' ') {
-  //         continue;
-  //       } else {
-  //         arrNew.push(members[i][j].toUpperCase());
-  //         break;
-  //       }
-  //     }
-  //   }
-  // }
-  // if (arrNew.length == 0) {
-  //   return false;
-  // } else {
-  //   console.log(arrNew);
-  //   return arrNew.sort().join('');
-  // }
+  let arrNew = [];
+  if (members == null) return false;
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      for (let j = 0; j < members[i].length; j++) {
+        if (members[i][j] == ' ') {
+          continue;
+        } else {
+          arrNew.push(members[i][j].toUpperCase());
+          break;
+        }
+      }
+    }
+  }
+  if (arrNew.length == 0) {
+    return false;
+  } else {
+    console.log(arrNew);
+    return arrNew.sort().join('');
+  }
 }
 
 module.exports = {
